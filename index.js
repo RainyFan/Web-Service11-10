5a19032de132cff075f5a42033705437ea518196
Rainy <fany824@newschool.com>
2022-11-13 17:27:00 -0500

    11/14

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Time to rest")
+import { sentence } from 'txtgen'
+
+// Update the express app code below to this:
+app.get('/', (req, res) => {
+  const randomSentence = sentence()
+
+  res.send(randomSentence)
+})
+
+// Import the express library and assign it to a variable
+import express from 'express'
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/', (req, res) => {
+  res.send("Time to Rest Service")
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` )
+})
